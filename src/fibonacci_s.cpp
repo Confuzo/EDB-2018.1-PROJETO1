d1224b280bfd2e3b509e569c02ae0b7db35d7a4a
geraldojrcg <geraldojrcg@gmail.com>
2018-03-13 15:58:04 -0300

    erro corrigido na fibonacci search

--- src/fibonacci_s.cpp
+++ src/fibonacci_s.cpp
@@ -1,5 +1,5 @@
 #include "../include/header.h"
-
+/**< função auxiliar que calcula o menor entre dois números */
 int menor(int num1, int num2){
         if(num1 < num2){
           return num1;
@@ -7,36 +7,45 @@ int menor(int num1, int num2){
           return num2;
         }
 }
+/// Implements a fibonacci search on an array of integers.
+/*!
+ * \@param first Pointer to the first element in the range.
+ * \@param last Pointer past the last valid element in the range.
+ * \@param last Pointer past the last valid element in the range.
+ * \@param value The target value we are looking for within the range.
+ * \return A pointer to the target value within the range; or last, in case the value is not in the range.
+ */
 int *fibonacci_s(int *first, int *last,  int *abs_last, int value){
         auto lenght = (last - first);
-        auto ini (first);
         int fmin2 = 0;
         int fmin1 = 1;
         int fmin = fmin1 + fmin2;
         int offset = -1;
-        auto i = ini;
-        int * res;
+        auto i = (first);
+        
+        /**<inicia os números de fibonacci até o maior número ser menor que o tamanho do array*/ 
         while(fmin < lenght){
                 fmin2 = fmin1;
                 fmin1 = fmin;
                 fmin = fmin1 + fmin2; 
         }
-        while(fmin > 1){ 
-                *i = menor(offset+fmin2, lenght-1);
-                
-                if(*i < value){
+        /**< inicia a procura do valor enquanto o número de fibonacci seja maior que 1*/ 
+        while(fmin > 1){
+                /**< seta o endereço de memória de first mais menor entre o menor valor dos três 
+                 * números de fibonacci -1 e o lenght da matriz -1*/ 
+                i = (first + menor(offset+fmin2, lenght-1));    
+                if(*i == value){
+                        return i;
+                }else if( *i < value){
                         fmin = fmin1;
                         fmin1 = fmin2;
                         fmin2 = fmin - fmin1;
-                        offset = *i;
-                }else if(*i > value){
+                        offset =  *i;
+                }else{
                         fmin = fmin2;
                         fmin1 = fmin1 - fmin2;
                         fmin2 = fmin - fmin1;
-                }else{
-                        return i; 
-                }
-
+                } 
         }
         return last;
  
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 int main(){
     int* (*fp_array[])(int*, int*, int*, int) = {linear_s_i, binary_s_i, binary_s_r, ternary_s_i, ternary_s_r, jump_s, fibonacci_s};
     int A[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Data container.
-    int targets[] = { 0,1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 3, 59, 6}; // Target values for testing.
+    int targets[] = { 0,1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 3, 59, -6}; // Target values for testing.
 
     // Prints out the original data container.
     std::cout << "Array: [ ";
     std::copy( std::begin(A), std::end(A), std::ostream_iterator<int>( std::cout , " " ) );
     std::cout << "]\n";
-
+    auto ini (std::begin(A));
     // Executes several searchs in the data container.
     for(int i=0; i<7; i++){
         std::cout<< ">> RUNING FUNCTION "<< i <<std::endl;
@@ -18,8 +18,7 @@ int main(){
             // Look for target in the entire range.
             auto result = (*fp_array[i])(std::begin(A), std::end(A), std::end(A), e );
             // Process the result
-            if ( result != std::end(A) )
-            {
+            if ( result != std::end(A) ){
                 std::cout << ">>> Found \"" << e << "\" at position "
                             << std::distance(std::begin(A),result) << ".\n";
             }
