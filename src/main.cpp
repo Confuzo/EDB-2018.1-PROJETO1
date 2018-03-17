10dd01d58f0bf89fb27abab3e68f19b31a3a3478
Confuzo <abraaovld@gmail.com>
2018-03-17 14:01:45 -0300

    Comments in main file improved

--- src/main.cpp
+++ src/main.cpp
@@ -13,53 +13,48 @@ std::vector<int> preenche_vetor(std::vector<int> A){
 int main(int argc, char *argv[]){ /**<Função Main*/
     int num_amostras;
     int amostras = 25;
+    /*<Recebe o número de amostras do usuário, por padrão o valor é 25*/
     if(argc > 1 && argc < 3){
          num_amostras = atoi(argv[1]);
-         amostras = num_amostras;
+         if(num_amostras > 0 and num_amostras <= 25){
+          amostras = num_amostras;
+         }
     }
     int* (*fp_array[])(int*, int*, int*, int) = {linear_s_i, binary_s_i, binary_s_r, ternary_s_i, ternary_s_r, jump_s, fibonacci_s};
     std::vector<int> A; // Data container.
-    //int targets[] = { 0,1, 2, 3, 4, 5, 6, 7, 8, 9 , 10, 3, 59, -6}; // Target values for testing.
+
     int targets = -1;
     A.reserve(num_elements); //reserva na memória
-    //preenche o vetor com randons de 0 á 999999;
+    /**<preenche o vetor A com numerados já ordenados*/
     A = preenche_vetor(A);
     /**< ponteiro que aponta pro primeiro enderenço de memória de A */
     int * begin = &(*A.begin());
     /**< ponteiro que aponta pro ultimo enderenço de memória de A*/
     int * end = begin + 1000000;
- 
-    /**<Executa várias buscar no vetor de dados.*/
+
+    /**<Executa os 7 algoritmos de busca.*/
     std::ofstream myfile;
     std::stringstream name;
     for(int i=0; i<7; i++){
         name << "logs/logFUNCTION"<<i<<".csv";
         myfile.open(name.str());
         myfile << "Size,Time\n";
-        //myfile << ">> RUNING FUNCTION "<< i << " \n";
+
+        /**<Executa várias buscar no vetor de dados usando amostras diferentes.*/
         for( auto j(0); j < amostras; j++ ){
 
           auto aux = std::chrono::steady_clock::now();
           auto media = aux -aux;
-          //std::cout << "\t\t>>> " << std::chrono::duration <double, std::milli> (diff).count() << " ms" << std::endl;
-          for(auto l(0); l < 1; l++){
-            // Look for target in the entire range.
+
+          for(auto l(0); l < 100; l++){
+            /*<Mede o tempo de execução da função em relação a uma amostra de tamanho n, gerando assim uma média.*/
             auto start = std::chrono::steady_clock::now();
             auto result = (*fp_array[i])(begin, end, end, targets);
             auto finish = std::chrono::steady_clock::now();
             auto diff = finish - start;
             media = media + (diff-media)/(l+1);
-            // Process the result
-            /*if ( result != end ){
-                std::cout << ">>> Found \"" << targets << "\" at position "
-                            << std::distance(begin ,result) << ".\n";
-            }
-            else
-            {
-                std::cout << ">>> Value \"" << targets << "\" not found in array!\n";
-            }*/
+
           }
-            //std::cout << "\t\t>>> " << std::chrono::duration <double, std::milli> (media).count() << " ms" << std::endl;
             myfile  << *end << "," << std::chrono::duration <double, std::milli> (media).count() << "\n";
             end += 39960000;
         }
